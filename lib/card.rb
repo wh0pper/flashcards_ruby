f035da88e19c134a1efe1a46ce94b19e9fc9b3d5
Jared Clemmensen and Russell Hofvendahl <russell@>
2018-02-19 11:06:50 -0800

    pass initialize test

--- lib/card.rb
+++ lib/card.rb
@@ -1,24 +1,9 @@
-class Entry
-  @@entries = []
-  attr_accessor :word, :definitions
+class Card
+  attr_accessor :front, :back
 
-  def initialize(word)
-    @@entries.push(self)
-    @word = word
-    @definitions = []
+  def initialize(front, back)
+    @front = front
+    @back = back
   end
 
-  def self.list
-    @@entries
-  end
-
-  def self.find(word)
-    @@entries.each do |entry|
-      return entry if entry.word == word
-    end
-  end
-
-  def define(definition)
-    @definitions.push(definition)
-  end
 end
